84a3f7ee3e6d595174743a3b8ba1f2cf28fd78b4
Kiaran Lau <roboperson10@gmail.com>
2026-02-17 22:20:14 -0800

    hero test

--- app/components/custom/Hero415.tsx
+++ app/components/custom/Hero415.tsx
@@ -4,9 +4,8 @@ export function Hero415(){
     return (
         <section className="relative h-100 overflow-hidden bg-gray-800 rounded-md text-white mb-4">
             <div id="title header" className="flex flex-row items-center justify-center mx-4 max-w-full h-24">
-                <h1 className="text-lg">415 industries</h1>
+                <h1 className="text-5xl!">415 industries</h1>
             </div>        
         </section>
-        
     );
 }
